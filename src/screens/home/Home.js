c754b9232bcdab2cb99c0e37dd8a09651feb3448
Divyank Dubey <dubeydivyank@gmail.com>
2021-11-30 23:39:05 +0530

    Added 'Upcoming Movies' heading to the homepage

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Header from "../../common/header/Header";
+import "./Home.css";
 
 const Home = () => {
 	return (
 		<div>
 			<Header />
-			<h2>Home.js</h2>
+			<div className="upcomingMoviesHeading">Upcoming Movies</div>
 		</div>
 	);
 };
