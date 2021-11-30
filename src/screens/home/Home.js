c6764b89ea77d9ecd6c5625f658c5fb26bb1e6f9
Divyank Dubey <dubeydivyank@gmail.com>
2021-12-01 01:35:15 +0530

    Gridlist of Upcoming Movies Added

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../common/header/Header";
 import "./Home.css";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import { GridListTileBar } from "@material-ui/core";
 
-const Home = () => {
+export default function Home() {
+	//Getting upcoming movies from the DB
+	const [upcomingMoviesList, setUpcomingMoviesList] = useState([]);
+	useEffect(() => {
+		getUpcomingMoviesList();
+	});
+
+	async function getUpcomingMoviesList() {
+		const response = await fetch("/api/v1/movies?status=PUBLISHED");
+		const responseBody = await response.json();
+		setUpcomingMoviesList(responseBody.movies);
+	}
+
 	return (
 		<div>
 			<Header />
 			<div className="upcomingMoviesHeading">Upcoming Movies</div>
+
+			{/* Gridlist of Upcoming Movies */}
+			<GridList cols={6} style={{ flexWrap: "nowrap" }}>
+				{upcomingMoviesList.map((movie) => (
+					<GridListTile key={movie.id} style={{ height: "250px" }}>
+						<img src={movie.poster_url} alt={movie.title} />
+						<GridListTileBar title={movie.title} />
+					</GridListTile>
+				))}
+			</GridList>
 		</div>
 	);
-};
-
-export default Home;
+}
